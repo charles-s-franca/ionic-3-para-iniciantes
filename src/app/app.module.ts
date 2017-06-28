d908a508574668461be2804aa56ce03ef7344212
Charles Franca <charlesfranca@Charless-MacBook-Air.local>
2017-06-28 15:37:04 -0300

    acessando API moviedb

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,8 +10,10 @@ import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { HttpModule } from "@angular/http"
 import { FeedPageModule } from "../pages/feed/feed.module";
 import { IntroPageModule } from "../pages/intro/intro.module";
+import { MoovieProvider } from '../providers/moovie/moovie';
 
 @NgModule({
   declarations: [
@@ -25,7 +27,8 @@ import { IntroPageModule } from "../pages/intro/intro.module";
     BrowserModule,
     IonicModule.forRoot(MyApp),
     FeedPageModule,
-    IntroPageModule
+    IntroPageModule,
+    HttpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -38,7 +41,8 @@ import { IntroPageModule } from "../pages/intro/intro.module";
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    MoovieProvider
   ]
 })
 export class AppModule {}
--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { MoovieProvider } from "../../providers/moovie/moovie";
 
 /**
  * Generated class for the FeedPage page.
@@ -11,11 +12,30 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 @Component({
   selector: 'page-feed',
   templateUrl: 'feed.html',
+  providers: [
+      MoovieProvider
+  ]
 })
 export class FeedPage {
+  public objeto_feed = {
+      titulo:"Charles Franca",
+      data: "November 5, 1955",
+      descricao:"Estou criando um app incrivel...",
+      qntd_likes: 12,
+      qntd_comments: 4,
+      time_comment: "11h ago teste"
+  }
+
+  public lista_filmes = new Array<any>();
+
+
   public nome_usuario:string = "Charles Franca do Codigo";
-  
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+  constructor(
+      public navCtrl: NavController, 
+      public navParams: NavParams,
+      private movieProvider: MoovieProvider
+      ) {
   }
 
   public somaDoisNumeros(num1:number, num2:number): void{
@@ -23,7 +43,18 @@ export class FeedPage {
   }
 
   ionViewDidLoad() {
-      //this.somaDoisNumeros(10, 99);
+      this.movieProvider.getLatestMovies().subscribe(
+        data=>{
+            
+            const response = (data as any);
+            const objeto_retorno = JSON.parse(response._body);
+            this.lista_filmes = objeto_retorno.results;
+
+            console.log(objeto_retorno);
+        }, error => {
+            console.log(error);
+        }
+      )
   }
 
 }
